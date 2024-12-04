9eadeaaf8f4aeafd57bb9a21605a6b44f5c05056
Cristian <cdom127@wgu.edu>
2024-12-03 22:16:10 -0800

    Prepare processor function for database operations

--- src/services/processor.ts
+++ src/services/processor.ts
@@ -1,4 +1,10 @@
+import { postReview } from '../api/bsky';
 import { fetchRSSFeed } from '../api/pitchfork';
+import {
+  getAllGuids,
+  storeAndGetReviewId,
+  updateReviewWithPostUrl,
+} from './reviewManager';
 
 const TRACKS_RSS_URL = 'https://pitchfork.com/feed/feed-track-reviews/rss';
 const ALBUMS_RSS_URL = 'https://pitchfork.com/feed/feed-album-reviews/rss';
@@ -8,21 +14,26 @@ export const fetchAndProcessReviews = async () => {
     const trackReviews = await fetchRSSFeed(TRACKS_RSS_URL);
     const albumReviews = await fetchRSSFeed(ALBUMS_RSS_URL);
 
-    const reviews = [...trackReviews, ...albumReviews].sort(
+    const reviews = [...trackReviews, ...albumReviews];
+
+    reviews.sort(
       (a, b) => a.publishedDate.getTime() - b.publishedDate.getTime()
     );
 
-    // Get all stored guis for reviews
-    const postedGuids = [''];
+    // Get all stored guis for reviews in ascending order
+    const postedGuids = await getAllGuids();
 
     // Loop through the reviews and post new ones. If one gets posted then stop looping
     for (const review of reviews) {
       if (!postedGuids.includes(review.guid)) {
-        // Post review
+        //create & store review data on db
+        console.log('Posting review:', review);
+        const newReviewId = await storeAndGetReviewId(review);
+        //post review to bsky
+        const postUrl = await postReview(review);
+        //update review in db with bsky post url
+        await updateReviewWithPostUrl(newReviewId, postUrl);
 
-        // Mark review as posted
-
-        console.log('Posted Review URL');
         break;
       }
     }
