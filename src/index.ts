ab3c607ca55490ece74cd774eaae6d85b1db92c2
ci-dominguez <c.isaacdominguez@gmail.com>
2024-12-03 23:27:14 -0800

    Update cron job schedule

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,15 @@ import { LoginToBSky } from './api/bsky';
 const startBot = async () => {
   console.log('Starting Mar...');
   await LoginToBSky();
-  //10am and 4pm pst every day
-  // cron.schedule(
-  //   '0 10, 16 * * *',
-  //   async () => {
-  //     await fetchAndProcessReviews();
-  //   },
-  //   { timezone: 'America/Los_Angeles' }
-  // );
-  await fetchAndProcessReviews();
+
+  //6am, 2pm, and 4pm pst every day
+  cron.schedule(
+    '0 6,14,16 * * *',
+    async () => {
+      await fetchAndProcessReviews();
+    },
+    { timezone: 'America/Los_Angeles' }
+  );
 };
 
 startBot();
